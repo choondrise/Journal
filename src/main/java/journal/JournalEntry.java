aa1e9c6fdc0b5f8f1ae2ac21cd52ea3ab5a343c6
Choondrise <loboximax@gmail.com>
2021-12-13 13:20:26 +0100

    Change String thoughts variable to int spent

--- src/main/java/journal/JournalEntry.java
+++ src/main/java/journal/JournalEntry.java
@@ -1,5 +1,11 @@
+package journal;
+
 import java.util.Date;
 
+/**
+ *
+ * @author choondrise
+ */
 public class JournalEntry implements Comparable<JournalEntry> {
 
     private Date date;
@@ -7,63 +13,120 @@ public class JournalEntry implements Comparable<JournalEntry> {
     private Date start;
     private Date end;
     private String events;
-    private String thoughts;
+    private int spent;
 
-    public JournalEntry(Date date, double rating, Date start, Date end, String events, String thoughts) {
+    /**
+     *
+     * @param date
+     * @param rating
+     * @param start
+     * @param end
+     * @param events
+     * @param spent
+     */
+    public JournalEntry(Date date, double rating, Date start, Date end, String events, int spent) {
         this.date = date;
         this.rating = rating;
         this.start = start;
         this.end = end;
         this.events = events;
-        this.thoughts = thoughts;
+        this.spent = spent;
     }
 
+    /**
+     *
+     * @return
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     *
+     * @param date
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getRating() {
         return rating;
     }
 
+    /**
+     *
+     * @param rating
+     */
     public void setRating(double rating) {
         this.rating = rating;
     }
 
+    /**
+     *
+     * @return
+     */
     public Date getStart() {
         return start;
     }
 
+    /**
+     *
+     * @param start
+     */
     public void setStart(Date start) {
         this.start = start;
     }
 
+    /**
+     *
+     * @return
+     */
     public Date getEnd() {
         return end;
     }
 
+    /**
+     *
+     * @param end
+     */
     public void setEnd(Date end) {
         this.end = end;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getEvents() {
         return events;
     }
 
+    /**
+     *
+     * @param events
+     */
     public void setEvents(String events) {
         this.events = events;
     }
 
-    public String getThoughts() {
-        return thoughts;
+    /**
+     *
+     * @return
+     */
+    public int getSpent() {
+        return spent;
     }
 
-    public void setThoughts(String thoughts) {
-        this.thoughts = thoughts;
+    /**
+     *
+     * @param spent
+     */
+    public void setSpent(int spent) {
+        this.spent = spent;
     }
 
     @Override
